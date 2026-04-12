3b436cd1b86aa85a40fd2175c990a074c9896f35
ROHIT SHARMA <38538605+ROHIT318@users.noreply.github.com>
2026-04-12 13:22:57 +0000

    Front end file minor changes.

--- chat_app/chat_frontend.py
+++ chat_app/chat_frontend.py
@@ -12,8 +12,14 @@ with st.container(border=True):
         with st.chat_message(message['role']):
             st.write(message['message'])
 
-    if user_message := st.chat_input('Enter your messages here.'):
-        st.session_state['messages'].append({'role': 'user', 'message': user_message})
-        ai_message = sequential_chain.invoke({'message_history': st.session_state['messages'], 'query': user_message})
-        st.session_state['messages'].append({'role': 'assistant', 'message': ai_message})
-        st.rerun()
+    col_1, col_2 = st.columns(2, [1, 10])
+    
+    with col_1: 
+        files = st.file_uploader()
+
+    with col_2:
+        if user_message := st.chat_input('Enter your messages here.'):
+            st.session_state['messages'].append({'role': 'user', 'message': user_message})
+            ai_message = sequential_chain.invoke({'message_history': st.session_state['messages'], 'query': user_message})
+            st.session_state['messages'].append({'role': 'assistant', 'message': ai_message})
+            st.rerun()
