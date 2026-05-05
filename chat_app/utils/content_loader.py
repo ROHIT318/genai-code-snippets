579eff9aa910575915fe07b1134413d1f2931d56
Rohit Sharma <iro1hit@gmail.com>
2026-05-05 21:29:09 +0530

    Added new test case for content_loader.py

--- chat_app/utils/content_loader.py
+++ chat_app/utils/content_loader.py
@@ -42,7 +42,7 @@ class ContentLoader():
 if __name__ == '__main__':
     
     ############################################
-    #           JOB Page Loading               #
+    #           JOB Page Loading 1             #
     ############################################
     # url_list = ['https://usijobs.deloitte.com/en_US/careersUSI/JobDetail/USI-EH26-EA-GTLO-Sharepoint-Senior-Analyst/340662', 
     #             'https://usijobs.deloitte.com/en_US/careersUSI/JobDetail/USI-EH26-EA-GTLO-Analytics-Insight-Senior-Analyst/340661', 
@@ -51,9 +51,18 @@ if __name__ == '__main__':
     # content_loader = ContentLoader(url_list)
     # content_loader.display_all_documents()
 
+    ############################################
+    #           JOB Page Loading 2             #
+    ############################################
+    url_list = ['https://www.pwc.in/careers/experienced-jobs/description.html?wdjobreqid=280138WD&wdcountry=IND&jobtitle=Associate&wdjobsite=Global_Experienced_Careers&wdjd=simple', 
+                'https://www.pwc.in/careers/experienced-jobs/description.html?wdjobreqid=288872WD&wdcountry=IND&jobtitle=1-10yrs+Application+for+Cyber-+Kolkata+DN+57+-+RDC&wdjobsite=Global_Experienced_Careers&wdjd=simple', 
+                'https://www.pwc.in/careers/experienced-jobs/description.html?wdjobreqid=439388WD&wdcountry=IND&jobtitle=Associate&wdjobsite=Global_Experienced_Careers&wdjd=simple'
+                ]
+    content_loader = ContentLoader(url_list)
+    content_loader.display_all_documents()
 
-    ############################################
-    #              PDF Loading                 #
-    ############################################
-    uploaded_pdf_loader = ContentLoader(url_list=[os.path.abspath('files/gradient_descent.pdf')])
-    uploaded_pdf_loader.display_all_documents()
+    # ############################################
+    # #              PDF Loading                 #
+    # ############################################
+    # uploaded_pdf_loader = ContentLoader(url_list=[os.path.abspath('files/gradient_descent.pdf')])
+    # uploaded_pdf_loader.display_all_documents()
